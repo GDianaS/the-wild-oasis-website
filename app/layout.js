df6397d30790e65a2259dc721de8e40fdc0176e1
G. Diana <gab13ssousa@gmail.com>
2025-11-01 17:41:01 -0300

    Header and Nav

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ const josefin = Josefin_Sans({
 //console.log(josefin)
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 // metadata quando em uma página específica, sobreescreve o metadata do layout
 export const metadata={
@@ -27,15 +28,15 @@ export default function RootLayout({children}){
     return(
         <html>
             <body className={`${josefin.className} 
-            bg-primary-950 text-primary-100 min-h-screen`}>
-                <header>
-                    <Logo/>
-                </header>
-                <Navigation />
-                <main>
-                    {children}
-                </main>
-                <footer> Copyright by The Wild Oasis</footer>
+            bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+                
+                <Header/>
+
+                <div className="flex-1 px -8 py-12">
+                    <main className="max-w-7xl mx-auto">
+                        {children}
+                    </main>
+                </div>
             </body>
         </html>
     )
