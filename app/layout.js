34ed9d1dc9a6138a639e34e4c2820dbc1b55b4a7
G. Diana <gab13ssousa@gmail.com>
2025-11-01 17:11:47 -0300

    Styles + Metadata + Icon

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,22 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 
+import "@/app/_styles/globals.css";
+
+// metadata quando em uma página específica, sobreescreve o metadata do layout
 export const metadata={
-    title:"The Wild Oasis",
+    //title:"The Wild Oasis", //título aba navegador
+    title:{
+        template:"%s / The Wild Oasis", //%s placeholder
+        default: "Welcome / The Wild Oasis"
+    },
+    description: "Luxurious cabin hotel located in the heart of the Italian Dolomites, surrounded by beautiful montains and dark forests."
 };
 
 export default function RootLayout({children}){
     return(
         <html>
-            <body>
+            <body className="bg-primary-950 text-primary-100 min-h-screen">
                 <header>
                     <Logo/>
                 </header>
