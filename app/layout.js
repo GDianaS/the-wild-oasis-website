37e8066b5bc8a11f874f23721fbe88f91ce57d34
G. Diana <gab13ssousa@gmail.com>
2025-11-01 18:10:35 -0300

    HomePage

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({children}){
     return(
         <html>
             <body className={`${josefin.className} 
-            bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+            bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
                 
                 <Header/>
 
--- app/page.js
+++ app/page.js
@@ -1,13 +1,31 @@
+// HOME PAGE
+import Image from "next/image";
 import Link from "next/link";
+import bg from "@/public/bg.png"; //permite que não se precise especificar o height e width em <Image/>
 
 export default function Page() {
   return(
-    <div>
-      <h1>The Wild Oasis. Welcome to paradise.</h1>
+    <main className="mt-24">
+      {/* <img
+        src="/bg.png"
+        alt="Mountains and forests with two cabins"
+      /> */}
+      <Image 
+        src={bg}
+        placeholder="blur"
+        quality={80}
+        fill className="object-cover object -top" //preenche todo o elemento (main) 
+        alt="Mountains and forests with two cabins"/>
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">Welcome to paradise</h1>
+        <Link 
+          href="/cabins" 
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all">
+        Explore luxury cabins
+        </Link>
+      </div>
 
-      <Link href="/cabins">Explore luxury cabins</Link>
-      
-    </div>
+    </main>
         
   );
 }
