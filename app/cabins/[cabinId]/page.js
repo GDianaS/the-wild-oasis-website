c212b8929d533acd20ad1028a0380639d83d2c56
G. Diana <gab13ssousa@gmail.com>
2025-11-05 22:27:17 -0300

    GenetateStaticParams to pre-render a Static Page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -21,6 +21,18 @@ export async function generateMetadata({params}){
     return {
         title: `Cabin ${name}`
     }
+}
+
+//Making dynamic pages static =: Pre-Render Pages
+export async function generateStaticParams(){
+  const cabins = await getCabins();
+
+  const ids = cabins.map(cabin => ({
+    cabinId: String(cabin.id)}));
+
+  //console.log(ids); //Ids to pre-render
+
+  return ids;
 }
 
 export default async function Page({params}) {
