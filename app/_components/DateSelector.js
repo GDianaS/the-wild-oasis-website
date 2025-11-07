c06a2cd3c7badc276a4a2821fd3497dd4bb5ca64
G. Diana <gab13ssousa@gmail.com>
2025-11-07 20:41:57 -0300

    Reservation Section: Data Fetching Strategies (Data Picker and Reservation Form)

---
+++ app/_components/DateSelector.js
@@ -0,0 +1,91 @@
+"use client"; //react-day-picker
+
+import { isWithinInterval } from "date-fns";
+import { DayPicker } from "react-day-picker"; //npm i react-day-picker
+import "react-day-picker/dist/style.css";
+
+function isAlreadyBooked(range, datesArr) {
+  return (
+    range.from &&
+    range.to &&
+    datesArr.some((date) =>
+      isWithinInterval(date, { start: range.from, end: range.to })
+    )
+  );
+}
+
+function DateSelector({settings, cabin, bookedDates}) {
+  // CHANGE
+  const regularPrice = 23;
+  const discount = 23;
+  const numNights = 23;
+  const cabinPrice = 23;
+  const range = { from: null, to: null };
+
+  // SETTINGS
+  const {minBookingLength, maxBookingLength} = settings;
+
+  // Define o último mês de navegação (Dezembro, 5 anos no futuro)
+  const fiveYearsFromNow = new Date(new Date().getFullYear() + 5, 11);
+
+  return (
+    <div className="flex flex-col justify-between">
+
+      <div className="flex justify-center">
+        <DayPicker
+          className="pt-12 mx-auto"
+          mode="range"
+          min={minBookingLength + 1} //minimum number of nights in the range
+          max={maxBookingLength} //maximum number of nights in the range
+          startMonth={new Date()}
+          //fromDate={new Date()}
+          endMonth={fiveYearsFromNow}
+          disabled={{ before: new Date() }} //Disable dates before a specific date
+          hideNavigation
+          captionLayout="dropdown"
+          numberOfMonths={2}
+        />
+      </div>
+
+      <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
+        <div className="flex items-baseline gap-6">
+          <p className="flex gap-2 items-baseline">
+            {discount > 0 ? (
+              <>
+                <span className="text-2xl">${regularPrice - discount}</span>
+                <span className="line-through font-semibold text-primary-700">
+                  ${regularPrice}
+                </span>
+              </>
+            ) : (
+              <span className="text-2xl">${regularPrice}</span>
+            )}
+            <span className="">/night</span>
+          </p>
+          {numNights ? (
+            <>
+              <p className="bg-accent-600 px-3 py-2 text-2xl">
+                <span>&times;</span> <span>{numNights}</span>
+              </p>
+              <p>
+                <span className="text-lg font-bold uppercase">Total</span>{" "}
+                <span className="text-2xl font-semibold">${cabinPrice}</span>
+              </p>
+            </>
+          ) : null}
+        </div>
+
+        {range.from || range.to ? (
+          <button
+            className="border border-primary-800 py-2 px-4 text-sm font-semibold"
+            onClick={() => resetRange()}
+          >
+            Clear
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default DateSelector;
